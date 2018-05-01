46f4dc2664cc5a9f518c12c7a924f2bb724ecaf3
Nolan Neeley <nolanneeley@gmail.com>
2018-05-01 05:23:33 -0500

    add mp3 upload to episodes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_30_173345) do
+ActiveRecord::Schema.define(version: 2018_05_01_101330) do
 
   create_table "episodes", force: :cascade do |t|
     t.string "title"
@@ -22,6 +22,10 @@ ActiveRecord::Schema.define(version: 2018_04_30_173345) do
     t.string "episode_thumbnail_content_type"
     t.integer "episode_thumbnail_file_size"
     t.datetime "episode_thumbnail_updated_at"
+    t.string "mp3_file_name"
+    t.string "mp3_content_type"
+    t.integer "mp3_file_size"
+    t.datetime "mp3_updated_at"
     t.index ["podcast_id"], name: "index_episodes_on_podcast_id"
   end
 
